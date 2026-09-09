7d4cd9e23382f20418f880966b0f2c90c2a06997
Ishaan Sharma <ishaanshama93@gmail.com>
2026-09-09 23:18:25 +0530

    Time: 9 ms (7.43%) | Memory: 46.9 MB (5.44%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,25 +1,30 @@
 class Solution {
     List<List<Integer>> ans;
+    Set<List<Integer>> st;
 
     private void helper(int[] arr, int start, int len, int target, List<Integer> list) {
+        if (target < 0 || start >= len) {
+            return;
+        }
+
         if (target == 0) {
-            ans.add(new ArrayList<>(list));
+            if (!st.contains(list)) {
+                ans.add(new ArrayList<>(list));
+                st.add(new ArrayList<>(list));
+            }
             return;
         }
 
-        if (target < 0) {
-            return;
-        }
-
-        for (int i = start; i < len; i++) {
-            list.add(arr[i]);
-            helper(arr, i, len  , target - arr[i], list);
-            list.remove(list.size() - 1);
-        }
+        list.add(arr[start]);
+        helper(arr, start + 1, len, target - arr[start], list);
+        helper(arr, start, len, target - arr[start], list);
+        list.remove(list.size() - 1);
+        helper(arr, start + 1, len, target, list);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         ans = new ArrayList<>();
+        st = new HashSet<>();
         helper(candidates, 0, candidates.length, target, new ArrayList<>());
         return ans;
     }
